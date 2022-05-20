66156a8429017125a91d6f7e5beb024ddcbda736
pdkary <pdkary@gmail.com>
2022-05-20 17:22:52 -0400

    added flask app

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ def get_random_words(n=1):
 
 def get_n_words_with_m_or_more_synonyms(n=10,m=2):
   words = get_random_words(n)
+  print(words)
   valid_words = {k:v for k,v in get_all_synonyms(words).items() if len(v) > m}
   if len(valid_words) == n:
     return valid_words
@@ -16,11 +17,12 @@ def get_n_words_with_m_or_more_synonyms(n=10,m=2):
 
 class GameState():
     def __init__(self):
-        wordset = get_n_words_with_m_or_more_synonyms(10,3)
-        self.starting_word, self.ending_word = sample(list(wordset.keys()),k=2)
+        # wordset = get_n_words_with_m_or_more_synonyms(2,2)
+        self.starting_word, self.ending_word = get_n_words_with_m_or_more_synonyms(2,4)
         self.path = [self.starting_word]
         self.current_word = self.starting_word
-        self.previous_word = None
+        self.current_syns = get_synonyms(self.current_word)
+
         print("="*10 + "GAME START" + "="*10)
         print("="*5 + self.starting_word.upper() + ">"*5 + self.ending_word.upper() + "="*5)
 
@@ -32,24 +34,20 @@ class GameState():
     def win(self):
         return self.ending_word in self.path
     
-    def play_round(self):
-        syns = get_synonyms(self.current_word)
-        print(self.path,": ",syns)
-        next_word = input("Pick a synonym: ")
+    def guess(self,next_word):
         if next_word.lower() == 'back':
             self.back()
-            self.play_round()
         else:
-            assert next_word in syns
+            assert next_word in self.current_syns
             self.path.append(next_word)
-            self.previous_word = self.current_word
             self.current_word = next_word
+            self.current_syns = [s for s in get_synonyms(self.current_word) if s not in self.path]
     
     def back(self):
         self.path = self.path[:-1]
-        self.current_word = self.previous_word
-
-
+        self.current_word = self.path[-1]
+        self.current_syns = [s for s in get_synonyms(self.current_word) if s not in self.path]
+    
 if __name__ == '__main__':
     gamestate = GameState()
     while not gamestate.win:
